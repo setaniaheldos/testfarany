b5e1a5fb5a456459dee5635046295466fac8017c
setaniaheldos <setaniaheldasseva@gmail.com>
2025-12-13 16:06:00 +0300

    Message de commit

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
--- src/components/admin/AdminAuth.jsx
+++ src/components/admin/AdminAuth.jsx
@@ -19,7 +19,7 @@ const AdminAuth = () => {
     setSuccess('');
     setLoading(true);
     try {
-      const res = await axios.post('http://82.165.15.45:5000/admins/login', form);
+      const res = await axios.post('http://82.165.15.45/admins/login', form);
       setLoading(false);
       if (res.status === 200) {
         setSuccess('Connexion admin réussie !');
